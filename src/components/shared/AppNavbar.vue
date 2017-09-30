8df4e7a282fd9dc3eafef0a531dd1323d4622815
Ronalson Filho <contact@ronalson.com.br>
2017-09-29 23:51:16 -0300

    Add navbar component w/o logic

---
+++ src/components/shared/AppNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="c-navbar">
+    <div class="c-navbar__logo">
+      <p>FLIP</p><span>BOOKS</span>
+    </div>
+    <div class="c-navbar__cart-counter">
+      <p>0</p>
+      <span><img src="../../assets/shopping-cart-icon.svg" alt="Shopping Cart"></span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
