83d0964346df56fd4226faf8a296373ddb4687a0
Ronalson Filho <contact@ronalson.com.br>
2017-09-30 21:15:48 -0300

    Add layout to shopping cart.

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,28 +1,33 @@
 <template>
   <main class="l-shopping-cart">
     <section v-for="product in products" class="c-cart">
-      <div class="c-cart-item">
-        <figure class="c-cart-item__image">
+      <div class="c-cart__item">
+        <figure class="c-cart__item__image">
           <img v-if="product.imageUrl" v-bind:src="product.imageUrl" alt="Product image">
           <img v-else src="../assets/product_placeholder.svg" alt="Product image">
         </figure>
-        <h2 class="c-cart-item__title">{{product.title}}</h2>
-        <p class="c-cart-item__author">{{product.author}}</p>
-        <div class="c-cart-item__counter">
+        <div class="c-cart__item__info">
+          <h2>{{product.title}}</h2>
+          <p>{{product.author}}</p>
+        </div>
+        
+        <div class="c-cart__item__quantity">
+          <h4>Quantidade</h4>
           <button>-</button>
           <input type="number">
           <button>+</button>
         </div>
-        <div class="c-cart-item__subtotal">
+        <div class="c-cart__item__subtotal">
           <h4>Subtotal</h4>
-          <p>product.price * item__counter</p>
+          <p>R$ 20,90</p>
         </div>
-        <p class="c-cart-item__btn--remove">Remover item</p>
+        <p class="c-cart__item__btn--remove">Remover item</p>
       </div>
-      <hr>
       <p class="c-cart__btn--keep-shopping">Continuar Comprando</p>
-      <p class="c-cart__price">Total</p>
-      <p class="c-cart__price">R$ 100,00</p>
+      <div class="c-cart__total">
+        <h4>Total</h4>
+        <p>R$ 100,00</p>
+      </div>
       <button class="c-cart__btn--buy">Finalizar</button>
     </section>
   </main>
